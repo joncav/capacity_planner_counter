742b34cf0cb3053f6db9f715fa6baa8f82ce6a6f
Jon Cavanaugh <Joncavanaugh@gmail.com>
2018-10-29 15:34:39 -0500

    Make sentence case standardized

--- capacity_planner_counter.sh
+++ capacity_planner_counter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#Get the counts for hosts, services, and check intervals for Capacity Planner
+# Get the counts for hosts, services, and check intervals for Capacity Planner
 
-#count of hosts
+# Count of hosts
 CHOSTS="$(mon query ls hosts -c name | wc -l)"
 
-#count of services
+# Count of services
 CSERVICES="$(mon query ls services -c description | wc -l)"
 
-#present the unique count of various check intervals for services
-#results are line separated as count,interval
+# Present the unique count of various check intervals for services
+# Results are line separated as count,interval
 UINTERVALS="$(mon query ls services -c check_interval | grep -o '^[0-9]*\.[0-9]' | sort | uniq -c | awk '{ print $1 "," $2}')"
 
 echo $1 - HOSTS:$CHOSTS, SERVICES:$CSERVICES, UNIQUE INTERVALS:$UINTERVALS
