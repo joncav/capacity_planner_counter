68c0e83f4cb166d5a78212eb6c629ecc26ba88bb
Jon Cavanaugh <Joncavanaugh@gmail.com>
2018-10-29 15:54:46 -0500

    Added nagios formatted output
    Added check interval average as well as formatted the other values into nagios friendly output.

--- capacity_planner_counter.sh
+++ capacity_planner_counter.sh
@@ -10,5 +10,8 @@ CSERVICES="$(mon query ls services -c description | wc -l)"
 # Present the unique count of various check intervals for services
 # Results are line separated as count,interval
 UINTERVALS="$(mon query ls services -c check_interval | grep -o '^[0-9]*\.[0-9]' | sort | uniq -c | awk '{ print $1 "," $2}')"
+# Calculate average check interval
+(( $(wc -l file1.txt) - $(wc -l file2.txt) ))
+AVGINTERVALS="$(mon query ls services -c check_interval | wc -c) / $CSERVICES)"
 
-echo $1 - HOSTS:$CHOSTS, SERVICES:$CSERVICES, UNIQUE INTERVALS:$UINTERVALS
+echo $1 - HOSTS:$CHOSTS, SERVICES:$CSERVICES, UNIQUE INTERVALS:$UINTERVALS | hosts=$CHOSTS services=$CSERVICES avg_intervals=$AVGINTERVALS
